a74594505a32f09588081f7e94873bdb4ddfc034
Michael Emhofer <emi@fsinf.at>
2013-04-28 21:16:59 +0200

    Added associations

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -1,3 +1,4 @@
 class Developer < ActiveRecord::Base
   attr_accessible :brand, :name
+  has_many :development_times
 end
--- app/models/film.rb
+++ app/models/film.rb
@@ -1,3 +1,4 @@
 class Film < ActiveRecord::Base
   attr_accessible :brand, :iso, :name
+  has_many :development_times
 end
