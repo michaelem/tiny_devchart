b26af89ef355ab2b7f00e36d162ddb68c5dd8b20
Michael Emhofer <emi@fsinf.at>
2013-04-28 19:43:21 +0200

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TinyDevchart::Application.config.secret_token = '32c80efea21755dae1f21dcb58d807d13d7df715db8755c763228ab2207ae4ef802733fcc8079e193f4e8792ce7d4151101186774f3403d65b77fa2ed8f8f2c5'
