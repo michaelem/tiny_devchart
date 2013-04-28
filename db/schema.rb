384b2f2e64bdb960001da6effc905eae4a4fd56b
Michael Emhofer <emi@fsinf.at>
2013-04-28 20:32:48 +0200

    migrated db

---
+++ db/schema.rb
@@ -0,0 +1,44 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended to check this file into your version control system.
+
+ActiveRecord::Schema.define(:version => 20130428180417) do
+
+  create_table "developers", :force => true do |t|
+    t.string   "name"
+    t.string   "brand"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "development_times", :force => true do |t|
+    t.integer  "seconds"
+    t.string   "dillution"
+    t.integer  "iso"
+    t.float    "temperature"
+    t.integer  "film_id"
+    t.integer  "developer_id"
+    t.string   "notes"
+    t.integer  "rating"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+  end
+
+  create_table "films", :force => true do |t|
+    t.string   "name"
+    t.integer  "iso"
+    t.string   "brand"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+end
