904908764814d7cf98caca28452e5b07601e6ba7
Michael Emhofer <emi@fsinf.at>
2013-06-03 18:00:28 +0200

    switched to enviroment variable for secret, so nobody deployes from the (not so) secret_token in the repo.

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,7 @@ module TinyDevchart
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.secret_token = ENV['TINY_DEVCHART_TOKEN']
   end
 end
