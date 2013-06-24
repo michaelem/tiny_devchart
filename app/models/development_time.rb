821cb896708dbeec49bbd2c90931148a39fb1531
Michael Emhofer <emi@fsinf.at>
2013-06-24 21:07:16 +0200

    layout improvements.

--- app/models/development_time.rb
+++ app/models/development_time.rb
@@ -2,4 +2,13 @@ class DevelopmentTime < ActiveRecord::Base
   attr_accessible :developer_id, :dillution, :film_id, :iso, :notes, :rating, :seconds, :temperature
   belongs_to :film
   belongs_to :developer
+
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.rating = 3
+    self.iso = 100
+    self.temperature = 20
+  end
 end
